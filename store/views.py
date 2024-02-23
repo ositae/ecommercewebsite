f8b4840a17df52a1a1842db41b8a8b142ea02794
Your Name <you@example.com>
2024-02-23 01:49:59 -0500

    feat: user registration complete

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 def home(request):
     products = Product.objects.all()
@@ -30,6 +34,26 @@ def logout_user(request):
     logout(request)
     messages.info(request, "Logged out successfully!")
     return redirect('home')
+
+def register_user(request):
+
+    return render(request, 'register.html', {})
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            messages.info(request, f"Account created for {username}! You are now logged in.")
+            return redirect('home')
+    else:
+        messages.error(request, "Please correct the error below.")
+        return render(request, 'register.html', { 'form': form })
 
 
 
